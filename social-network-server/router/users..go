b9aa30b61451f2ea98b6a13fea3ea3b5193a856c
Pravdin Ivan <pravdin.biss@gmail.com>
2022-12-24 19:36:22 +0400

    Improve users endpoint

--- social-network-server/router/users..go
+++ social-network-server/router/users..go
@@ -8,14 +8,21 @@ import (
 )
 
 func (h handler) getUser(c *gin.Context) {
-
+	id_str := c.Query("id")
 	db := h.DB
 	var user models.User
-	id, err := strconv.Atoi(c.Query("id"))
-	if err != nil {
-		return
+	if len(id_str) == 0 {
+		user.Username = c.Query("username")
+		db.Preload("Posts").Preload("Followers").First(&user).Where("username = ?", user.Username)
+	} else {
+		id, err := strconv.Atoi(id_str)
+		if err != nil {
+			return
+		}
+		user.ID = uint(id)
+
+		db.Preload("Posts").Preload("Followers").First(&user).Where("id = ?", user.ID)
 	}
-	user.ID = uint(id)
-	db.Preload("Posts").Preload("Followers").First(&user).Where("id = ?", user.ID)
+
 	c.IndentedJSON(http.StatusOK, user)
 }
