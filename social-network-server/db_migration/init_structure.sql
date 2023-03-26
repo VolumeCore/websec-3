59273b2b738909902afd7314fba8dafb7d648a49
Pravdin Ivan <ivan.pravdin@netcracker.com>
2023-03-26 21:53:43 +0400

    Add k8s manifests

--- social-network-server/db_migration/init_structure.sql
+++ social-network-server/db_migration/init_structure.sql
@@ -1,29 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `social_network_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `social_network_db` ;
 USE `social_network_db`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: social_network_db
--- ------------------------------------------------------
--- Server version	8.0.31
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `followers`
---
 
 DROP TABLE IF EXISTS `followers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `followers` (
                              `id` bigint unsigned NOT NULL AUTO_INCREMENT,
                              `created_at` datetime(3) DEFAULT NULL,
@@ -35,16 +14,11 @@ CREATE TABLE `followers` (
                              KEY `idx_followers_deleted_at` (`deleted_at`),
                              KEY `fk_users_followers` (`user_id`),
                              CONSTRAINT `fk_users_followers` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
 
---
--- Table structure for table `posts`
---
 
 DROP TABLE IF EXISTS `posts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `posts` (
                          `id` bigint unsigned NOT NULL AUTO_INCREMENT,
                          `created_at` datetime(3) DEFAULT NULL,
@@ -57,16 +31,11 @@ CREATE TABLE `posts` (
                          KEY `idx_posts_deleted_at` (`deleted_at`),
                          KEY `fk_users_posts` (`user_id`),
                          CONSTRAINT `fk_users_posts` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
---
--- Table structure for table `users`
---
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `users` (
                          `id` bigint unsigned NOT NULL AUTO_INCREMENT,
                          `created_at` datetime(3) DEFAULT NULL,
@@ -75,19 +44,10 @@ CREATE TABLE `users` (
                          `username` varchar(20) DEFAULT NULL,
                          `password_hash` bigint unsigned DEFAULT NULL,
                          `refresh_token` longtext,
+                         `bio` longtext,
+                         `image_uid` longtext,
                          PRIMARY KEY (`id`),
                          UNIQUE KEY `username` (`username`),
                          KEY `idx_users_deleted_at` (`deleted_at`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-12-22  2:03:29
